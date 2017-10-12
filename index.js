eb3c2ba916ab19db8fcf556d00ff279e27e1f108
Timothy Cevallos <timcev@gmail.com>
2017-10-12 21:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function showIssues(json) {
 function createIssue() {
   const repo = `https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/issues`
   const data = { title: issueTitle, body: issueBody }
-   fetch(`${baseApi}repos/${fork}/issues`, {
+   fetch(repo, {
      method: 'post',
      headers: {
        'Authorization': `token ${getToken()}`
