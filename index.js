146929c6456b48fa9bb6aa7e11e7ee78fce2d4de
Timothy Cevallos <timcev@gmail.com>
2017-10-12 21:11:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/issues'
   //use fetch to fork it!
-  fetch(`${baseApi}repos/${repo}/forks`, {
+  fetch(repo, {
     method: 'post',
     headers: {
       'Authorization': `token ${getToken()}`
