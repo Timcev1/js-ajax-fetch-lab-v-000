791fea4846471124365ea57ab318a5631d6eb0ba
Timothy Cevallos <timcev@gmail.com>
2017-10-12 21:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ function showIssues(json) {
 
 function createIssue() {
   const repo = `https://api.github.com/repos/learn-co-students/javascript-fetch-lab/issues`
-  const data = {
-    title: document.getElementById('title').value,
-    body: document.getElementById('body').value
-  };
+  const data = { title: issueTitle, body: issueBody }
+   fetch(`${baseApi}repos/${fork}/issues`, {
+     method: 'post',
+     headers: {
+       'Authorization': `token ${getToken()}`
+     },
+     body: JSON.stringify(data)
+   }).then(resp => getIssues())
 }
 
 function showResults(json) {
