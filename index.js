95f02b6f014051a6b06b6efc9d2b1da766d609db
Timothy Cevallos <timcev@gmail.com>
2017-10-12 20:59:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  const repo = `https://api.github.com/repos/learn-co-students/javascript-fetch-lab/issues`
 }
 
 function showResults(json) {
